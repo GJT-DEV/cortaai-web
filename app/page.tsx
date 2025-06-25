e2ebeac7c8cedd17b8abc03c9a14b0d249efa651
Gustavo Silva <gustavost1y@gmail.com>
2025-06-25 10:10:51 -0300

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,106 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+const slides = [
+  {
+    title: "Agendamento",
+    description:
+      "Não perca mais tempo em filas de espera ou ligações, agende os seus compromissos com agilidade e comodidade!",
+    image: "/onboard/agendamento.jpg",
+  },
+  {
+    title: "Pagamento Online",
+    description:
+      "Pague pelo seu serviço diretamente pelo aplicativo, de forma segura e fácil. Sem a necessidade de levar dinheiro em mãos.",
+    image: "/onboard/pagamento_online.jpg",
+  },
+  {
+    title: "Programa de Fidelidade",
+    description:
+      "Acumule pontos a cada serviço realizado e troque por bônus exclusivos. Cuide de si mesmo e ganhe benefícios!",
+    image: "/onboard/programa_fidelidade.jpg",
+  },
+];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+export default function OnBoard() {
+  const [step, setStep] = useState(0);
+  const [hasStarted, setHasStarted] = useState(false);
+
+  const handleNext = () => {
+    if (step < slides.length - 1) {
+      setStep(step + 1);
+    } else {
+      console.log("Começar agora");
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setStep((prev) => (prev >= slides.length ? 1 : prev + 1));
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="h-screen w-full bg-muted flex flex-col items-center justify-center text-white">
+      {!hasStarted ? (
+        <div className="flex flex-col items-center justify-center gap-6 px-8">
+          <div className="text-4xl font-bold tracking-wide">Corta Ai</div>
+          <Image src="/logo.svg" alt="Logo" width={80} height={80} />
+          <Button
+            onClick={() => {
+              setHasStarted(true);
+            }}
+            className="mt-10 text-white bg-blue-600"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Começar agora
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ) : (
+        <div className="flex flex-col justify-between h-full w-full max-w-md mx-auto align-bottom ">
+          <div className="absolute w-full h-[75%]">
+            <Image
+              src={slides[step - 1]?.image}
+              alt={slides[step - 1]?.title}
+              fill
+              className="object-fill"
+            />
+            <div className="absolute inset-0 bg-blue-900/20" />
+          </div>
+
+          <div className="z-100 h-full flex justify-end flex-col">
+            <div className="bg-blue-950 px-6 py-8 rounded-t-3xl flex flex-col h-fit">
+              <h2 className="text-xl font-semibold">
+                {slides[step - 1]?.title}
+              </h2>
+              <p className="text-sm mt-2 mb-6">
+                {slides[step - 1]?.description}
+              </p>
+
+              <div className="flex gap-2 mb-4 justify-center">
+                {slides.map((_, i) => (
+                  <span
+                    key={i}
+                    className={`h-2 rounded-full transition-all duration-300 ease-in-out
+        ${step - 1 === i ? "bg-white w-6" : "bg-white/30 w-2"}`}
+                  />
+                ))}
+              </div>
+
+              <Button
+                onClick={handleNext}
+                className="w-full bg-blue-600 text-white"
+              >
+                Começar agora
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
